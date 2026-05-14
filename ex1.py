d12dad8cafeb1d19b596e17dba122e3ac8296e27
llucenaa <gabrielramoslucenacampos@gmail.com>
2026-05-13 23:30:54 -0300

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,12 @@
+print("=" * 40)
+print("   CALCULADORA DE MÉDIA DE NOTAS")
+print("=" * 40)
+
+nota1 = float(input("\nDigite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota : "))
+
+media = (nota1 + nota2) / 2
+
+print(f"\nNota 1 : {nota1:.1f}")
+print(f"Nota 2 : {nota2:.1f}")
+print(f"Média  : {media:.2f}")
